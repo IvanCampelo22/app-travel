08e17678ed03bb391dd7ae9494450f34bef60048
Ivan <ivancampelo1973@gmail.com>
2023-01-16 22:05:17 -0300

    test(web/auth): refactor WebAuthClient unit test

--- libs/web/auth/specs/auth-client.spec.ts
+++ libs/web/auth/specs/auth-client.spec.ts
@@ -3,7 +3,9 @@ import { WebAuthClient } from '../src'
 describe('WebAuthClient', () => {
   describe('.instance', () => {
     it('should return a Client openid-client instance', async () => {
-      const client = (await WebAuthClient.getInstance()).client
+      const webAuth = await WebAuthClient.getInstance()
+
+      const client = webAuth.client
 
       expect(client).toBeDefined()
       expect(client.constructor.name).toBe('Client')
