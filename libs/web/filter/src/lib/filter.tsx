2b29314ee470ec75cc5686f7a6bbc8f71db92385
sal-rosa <danielvasconcelos148@gmail.com>
2023-01-26 23:10:56 -0300

    feat(web/filter): add filter component
    add filter component
    
    [page] Criar página de listagem de atendimentos #2

---
+++ libs/web/filter/src/lib/filter.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+
+import {
+  Button,
+  Container,
+  createStyles,
+  Flex,
+  Group,
+  MultiSelect
+} from '@mantine/core'
+
+import { DateRangePicker, DateRangePickerValue } from '@mantine/dates'
+
+import { IconCalendar, IconSearch } from '@tabler/icons'
+
+const useStyles = createStyles((theme) => ({
+  multiSelect: {
+    maxWidth: 500,
+    width: 500
+  }
+}))
+
+export function Filter() {
+  const { classes } = useStyles()
+
+  const [value, setValue] = useState<DateRangePickerValue>([
+    new Date(2021, 11, 1),
+    new Date(2021, 11, 5)
+  ])
+
+  return (
+    <Container fluid>
+      <Flex justify="space-between" wrap="wrap">
+        <MultiSelect
+          className={classes.multiSelect}
+          radius="md"
+          placeholder="Search"
+          searchable
+          icon={<IconSearch size={20} />}
+          data={[
+            'React',
+            'Angular',
+            'Svelte',
+            'Vue',
+            'Riot',
+            'Next.js',
+            'Blitz.js'
+          ]}
+        />
+        <Group>
+          <DateRangePicker
+            clearable={false}
+            icon={<IconCalendar size={20} />}
+            styles={(theme) => ({
+              input: {
+                fontWeight: 500,
+                minWidth: 225
+              }
+            })}
+            radius="md"
+            inputFormat="DD MMM, YYYY"
+            value={value}
+            onChange={setValue}
+          />
+          <Button variant="default" radius="md">
+            Filter
+          </Button>
+        </Group>
+      </Flex>
+    </Container>
+  )
+}
+
+export default Filter
