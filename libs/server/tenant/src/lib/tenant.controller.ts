fbcedbbe6d2f81960742af14adfa8888eef0afb6
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-01-29 20:57:05 -0300

    test(server/tenant): e2e tests created for the create, findmany and update methods, for the tenant

--- libs/server/tenant/src/lib/tenant.controller.ts
+++ libs/server/tenant/src/lib/tenant.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post
+} from '@nestjs/common'
 import { Prisma } from '@prisma/client'
 import { TenantService } from './tenant.service'
+;(BigInt.prototype as any).toJSON = function () {
+  return this.toString()
+}
 
 @Controller('tenants')
 export class TenantController {
@@ -8,21 +19,27 @@ export class TenantController {
 
   @Post()
   async create(@Body() input: Prisma.TenantCreateArgs) {
-    return await this.service.create(input)
+    return this.service.create(input)
   }
 
   @Get()
   async findMany() {
-    return await this.service.findMany()
-  }
-
-  @Patch('id')
-  async update(@Param('id') id: number, params: Prisma.TenantUpdateArgs) {
-    return await this.service.update({ ...params, where: { id } })
+    return this.service.findMany()
   }
 
   @Patch(':id')
-  async remove(id: bigint, isActive: false) {
-    return await this.service.deactivate(id, isActive)
+  async update(
+    @Param('id') id: number,
+    @Body('data') data: Prisma.TenantUpdateArgs
+  ) {
+    return this.service.update({
+      data,
+      where: { id: Number(id) }
+    })
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string, isActive: false) {
+    return this.service.deactivate(Number(id), isActive)
   }
 }
