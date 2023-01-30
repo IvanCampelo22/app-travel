b69a47d248f19e52302f1f3b18bb18162777b80c
sal-rosa <danielvasconcelos148@gmail.com>
2023-01-29 22:40:42 -0300

    fix(web/pagination): remove container to prevent wrong padding
    n

--- libs/web/pagination/src/lib/pagination.tsx
+++ libs/web/pagination/src/lib/pagination.tsx
@@ -1,22 +1,20 @@
-import { Button, Container, Flex, Group, Text } from '@mantine/core'
+import { Button, Flex, Group, Text } from '@mantine/core'
 
 export function Pagination() {
   return (
-    <Container fluid>
-      <Flex justify="space-between" align="center">
-        <Text weight="500" color="gray.7">
-          Page 1 of 10
-        </Text>
-        <Group>
-          <Button variant="default" radius="md">
-            Previus
-          </Button>
-          <Button variant="default" radius="md">
-            Next
-          </Button>
-        </Group>
-      </Flex>
-    </Container>
+    <Flex justify="space-between" align="center">
+      <Text weight="500" color="gray.7">
+        Page 1 of 10
+      </Text>
+      <Group>
+        <Button variant="default" radius="md">
+          Previus
+        </Button>
+        <Button variant="default" radius="md">
+          Next
+        </Button>
+      </Group>
+    </Flex>
   )
 }
 
