5212214277f85c06645b1ea405cf918303167f03
sal-rosa <danielvasconcelos148@gmail.com>
2023-01-29 22:39:56 -0300

    fix(web/page-header): remove container to prevent wrong padding
    n

--- libs/web/page-header/src/lib/page-header.tsx
+++ libs/web/page-header/src/lib/page-header.tsx
@@ -1,35 +1,31 @@
-import { Button, Container, Flex, Group, Title } from '@mantine/core'
+import { Button, Flex, Group, Title } from '@mantine/core'
 
 import { IconDownload, IconPlus } from '@tabler/icons'
 
-//export interface PageHeaderProps { }
-
 export function PageHeader() {
   return (
-    <Container fluid>
-      <Flex justify="space-between">
-        <div>
-          <Title order={1} size={30} color="gray.9" weight={500}>
-            Customer Service
-          </Title>
-          <Title order={5} color="gray.5" weight={400}>
-            View your trades and transactions.
-          </Title>
-        </div>
-        <Group>
-          <Button
-            leftIcon={<IconDownload size={18} />}
-            radius="md"
-            variant="default"
-          >
-            Export
-          </Button>
-          <Button color="blue.8" radius="md" leftIcon={<IconPlus size={18} />}>
-            Add
-          </Button>
-        </Group>
-      </Flex>
-    </Container>
+    <Flex justify="space-between">
+      <div>
+        <Title order={1} size={30} color="gray.9" weight={500}>
+          Customer Service
+        </Title>
+        <Title order={5} color="gray.5" weight={400}>
+          View your trades and transactions.
+        </Title>
+      </div>
+      <Group>
+        <Button
+          leftIcon={<IconDownload size={18} />}
+          radius="md"
+          variant="default"
+        >
+          Export
+        </Button>
+        <Button color="blue.8" radius="md" leftIcon={<IconPlus size={18} />}>
+          Add
+        </Button>
+      </Group>
+    </Flex>
   )
 }
 
