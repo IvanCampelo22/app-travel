fae2f41b43c6e07840b105826026b4bf908edbe1
sal-rosa <danielvasconcelos148@gmail.com>
2023-02-19 16:07:27 -0300

    refactor(web/childe-age): add others values to select

--- libs/web/childe-age/src/lib/childe-age.tsx
+++ libs/web/childe-age/src/lib/childe-age.tsx
@@ -20,7 +20,19 @@ export function ChildeAge() {
         { value: 'one', label: '1 Ano' },
         { value: 'two', label: '2 Anos' },
         { value: 'three', label: '3 Anos' },
-        { value: 'four', label: '4 Anos' }
+        { value: 'four', label: '4 Anos' },
+        { value: 'five', label: '5 Anos' },
+        { value: 'six', label: '6 Anos' },
+        { value: 'seven', label: '7 Anos' },
+        { value: 'eight', label: '8 Anos' },
+        { value: 'nine', label: '9 Anos' },
+        { value: 'ten', label: '10 Anos' },
+        { value: 'twelve', label: '12 Anos' },
+        { value: 'thirteen', label: '13 Anos' },
+        { value: 'fourteen', label: '14 Anos' },
+        { value: 'fifteen', label: '15 Anos' },
+        { value: 'sixteen', label: '16 Anos' },
+        { value: 'seventeen', label: '17 Anos' }
       ]}
     />
   )
