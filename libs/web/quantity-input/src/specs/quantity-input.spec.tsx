6b69a695b27a344e0475cebbb771b0c1394eb7f2
sal-rosa <danielvasconcelos148@gmail.com>
2023-02-19 16:20:24 -0300

    refactor(web/quantity-input): add testes

--- libs/web/quantity-input/src/specs/quantity-input.spec.tsx
+++ libs/web/quantity-input/src/specs/quantity-input.spec.tsx
@@ -1,16 +1,45 @@
 import '@testing-library/jest-dom'
-import { screen } from '@testing-library/react'
-import { renderWithTheme } from '@web/base-ui'
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from '@web/base-ui/utils'
 import QuantityInput from '../lib/quantity-input'
 
 describe('<QuantityInput />', () => {
   it('should render successfully with value 1', () => {
-    renderWithTheme(<QuantityInput />)
+    renderWithTheme(
+      <QuantityInput title="Teste" description="description" min={1} max={7} />
+    )
 
-    const element = screen.getByTestId('input')
+    const input = screen.getByTestId('input')
 
-    element.focus()
+    expect(input).toHaveValue('1')
+  })
+  it('should increase number', () => {
+    renderWithTheme(
+      <QuantityInput title="Teste" description="description" min={1} max={7} />
+    )
 
-    expect(element).toHaveValue('1')
+    const input = screen.getByTestId('input')
+    const buttonPlus = screen.getByTestId('plus')
+
+    fireEvent.click(buttonPlus)
+
+    expect(input).toHaveValue('2')
+  })
+  it('should decrease number', () => {
+    renderWithTheme(
+      <QuantityInput title="Teste" description="description" min={1} max={7} />
+    )
+
+    const input = screen.getByTestId('input')
+    const buttonPlus = screen.getByTestId('plus')
+    const buttonMinus = screen.getByTestId('minus')
+
+    fireEvent.click(buttonPlus)
+
+    expect(input).toHaveValue('2')
+
+    fireEvent.click(buttonMinus)
+
+    expect(input).toHaveValue('1')
   })
 })
