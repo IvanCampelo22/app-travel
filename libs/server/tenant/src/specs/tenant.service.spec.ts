b6ab072b5b9b4467d50d925dd2ca453281d70310
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-03-11 12:19:16 -0300

    test(server/tenant): add new code for test service with dtos

--- libs/server/tenant/src/specs/tenant.service.spec.ts
+++ libs/server/tenant/src/specs/tenant.service.spec.ts
@@ -8,12 +8,13 @@ import {
 import { UserModule, UserService } from '@server/user'
 import { TenantModule } from '../lib/tenant.module'
 import { TenantService } from '../lib/tenant.service'
-import { createOneTenant } from '../specs/tenant.fixtures'
+import { CreateTenantDto } from './../lib/dto/tenant.create.dto'
 
 describe('Tenant Service', () => {
   let tenantService: TenantService
   let userService: UserService
   let moduleRef: TestingModule
+  let db: DatabaseService
 
   beforeAll(async () => {
     moduleRef = await Test.createTestingModule({
@@ -25,6 +26,12 @@ describe('Tenant Service', () => {
 
     tenantService = moduleRef.get<TenantService>(TenantService)
     userService = moduleRef.get<UserService>(UserService)
+    db = moduleRef.get<DatabaseService>(DatabaseService)
+  })
+  beforeEach(async () => {
+    await db.account.deleteMany({})
+    await db.tenant.deleteMany({})
+    await db.user.deleteMany({})
   })
 
   afterAll(() => {
@@ -33,19 +40,29 @@ describe('Tenant Service', () => {
 
   describe('create', () => {
     it('should save and return a tenant object', async () => {
-      const { id: tenantId } = await tenantService.create(createOneTenant())
-
-      const user = await userService.find({ tenantId })
-
-      expect(user).toBeDefined()
-      expect(user?.isActive).toBeTruthy()
-      expect(user?.isMaster).toBeTruthy()
+      const createTenant: CreateTenantDto = {
+        name: 'tenant1',
+        email: 'tenant1@gmail.com'
+      }
+      const tenant = await tenantService.create(createTenant)
+      expect(tenant.name).toBe('tenant1')
     })
   })
 
   describe('findMany', () => {
     it('should return all tenants objects', async () => {
-      await tenantService.create(createOneTenant())
+      const createTenant1: CreateTenantDto = {
+        name: 'tenant1',
+        email: 'tenant1@gmail.com'
+      }
+
+      const createTenant2: CreateTenantDto = {
+        name: 'tenant2',
+        email: 'tenant2@gmail.com'
+      }
+
+      await tenantService.create(createTenant1)
+      await tenantService.create(createTenant2)
 
       const tenants = await tenantService.findMany()
 
@@ -55,9 +72,13 @@ describe('Tenant Service', () => {
 
   describe('find', () => {
     it('should return one tenant object', async () => {
-      const { id, email } = await tenantService.create(createOneTenant())
+      const createTenant: CreateTenantDto = {
+        name: 'tenant1',
+        email: 'tenant1@gmail.com'
+      }
+      const { id, email } = await tenantService.create(createTenant)
 
-      const tenant = await tenantService.find({ id })
+      const tenant = await tenantService.find(id)
 
       expect(tenant?.email).toEqual(email)
     })
@@ -65,13 +86,16 @@ describe('Tenant Service', () => {
 
   describe('update', () => {
     it('should update a tenant object', async () => {
+      const createTenant: CreateTenantDto = {
+        name: 'tenant1',
+        email: 'tenant1@gmail.com'
+      }
+
+      await tenantService.create(createTenant)
       const { id } = (await tenantService.findMany())[0]
 
-      const tenant = await tenantService.update({
-        where: { id },
-        data: {
-          email: 'tenant2@gmail.com'
-        }
+      const tenant = await tenantService.update(id, {
+        email: 'tenant2@gmail.com'
       })
 
       expect(tenant.email).toBe('tenant2@gmail.com')
@@ -80,6 +104,11 @@ describe('Tenant Service', () => {
 
   describe('destroy', () => {
     it('should destroy(soft delete) a tenant object', async () => {
+      const createObj: CreateTenantDto = {
+        name: 'Tenant1',
+        email: 'tenant1@gmail.com'
+      }
+      await tenantService.create(createObj)
       const mock = jest.spyOn(userService, 'destroy')
       const { id } = (await tenantService.findMany())[0]
 
