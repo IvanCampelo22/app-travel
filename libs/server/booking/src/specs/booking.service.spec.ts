51e2358602e3d147149292ab1c9fa73549137b73
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-03-11 12:58:50 -0300

    test(server/booking): refactor code for test service with dtos

--- libs/server/booking/src/specs/booking.service.spec.ts
+++ libs/server/booking/src/specs/booking.service.spec.ts
@@ -1,13 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import {
-  Account,
-  AccountCategory,
-  BookingStatus,
-  Tenant,
-  User
-} from '@prisma/client'
+import { Account, BookingStatus, Tenant } from '@prisma/client'
 import { AccountModule, AccountService } from '@server/account'
-import { createOneAccount } from '@server/account/fixtures'
 import { BookingModule } from './../lib/booking.module'
 import { BookingService } from './../lib/booking.service'
 
@@ -18,83 +11,85 @@ import {
   DatabaseTestService
 } from '@server/database'
 import { TenantModule, TenantService } from '@server/tenant'
-import { createOneTenant } from '@server/tenant/fixtures'
-import { UserModule, UserService } from '@server/user'
+import { UserModule } from '@server/user'
 describe('Booking Service', () => {
-  let moduleRef: TestingModule
-  let bookingService: BookingService
-  let tenantService: TenantService
-  let accountService: AccountService
-  let userService: UserService
-  let franchise: Tenant
-  let franchiseMasterUser: User
-  let agency: Account
-  let agencyMasterUser: User
+  jest.setTimeout(100000)
+})
+let moduleRef: TestingModule
+let bookingService: BookingService
+let tenantService: TenantService
+let accountService: AccountService
+//let userService: UserService
+let franchise: Tenant
+//let franchiseMasterUser: User
+let agency: Account
+//let agencyMasterUser: User
 
-  beforeAll(async () => {
-    moduleRef = await Test.createTestingModule({
-      imports: [
-        CoreModule,
-        DatabaseModule,
-        TenantModule,
-        UserModule,
-        AccountModule,
-        BookingModule
-      ]
-    })
-      .overrideProvider(DatabaseService)
-      .useClass(DatabaseTestService)
-      .compile()
-
-    tenantService = moduleRef.get<TenantService>(TenantService)
-    userService = moduleRef.get<UserService>(UserService)
-    accountService = moduleRef.get<AccountService>(AccountService)
-    bookingService = moduleRef.get<BookingService>(BookingService)
-
-    franchise = await tenantService.create(createOneTenant())
-    franchiseMasterUser = (await userService.find({ tenantId: franchise.id }))!
-    agency = await accountService.create(
-      createOneAccount(
-        franchise.id,
-        franchiseMasterUser.id,
-        AccountCategory.Agency
-      )
-    )
-    agencyMasterUser = (await userService.find({ accountId: agency.id }))!
+beforeAll(async () => {
+  moduleRef = await Test.createTestingModule({
+    imports: [
+      CoreModule,
+      DatabaseModule,
+      TenantModule,
+      UserModule,
+      AccountModule,
+      BookingModule
+    ]
   })
+    .overrideProvider(DatabaseService)
+    .useClass(DatabaseTestService)
+    .compile()
 
-  afterAll(() => {
-    moduleRef.close()
+  tenantService = moduleRef.get<TenantService>(TenantService)
+  //userService = moduleRef.get<UserService>(UserService)
+  accountService = moduleRef.get<AccountService>(AccountService)
+  bookingService = moduleRef.get<BookingService>(BookingService)
+
+  franchise = await tenantService.create({
+    name: 'tenant1',
+    email: 'tenant1@gmail.com'
   })
-
-  describe('new', () => {
-    it('should return a new booking object', async () => {
-      // const mock = jest
-      //   .spyOn(userService, 'getLoggedUser')
-      //   .mockImplementation(() =>
-      //     Promise.resolve({ ...agencyMasterUser, account: agency })
-      //   )
-
-      const booking = await bookingService.new()
-
-      expect(booking.id).toBeDefined()
-      expect(booking.tenantId).toEqual(franchise.id)
-      expect(booking.accountId).toEqual(agency.id)
-      expect(booking.ownerId).toEqual(agencyMasterUser.id)
-      expect(booking.status).toEqual(BookingStatus.WaitingService)
-      expect(booking.createdAt).toBeDefined()
-
-      jest.clearAllMocks()
-    })
-
-    // describe('', () => {
-    //   it('should ', async () => {
-    //     const booking = updateOneBooking(12)
-    //     const productArray = booking.data.products?.createMany
-    //       ?.data as Array<Prisma.BookingProductCreateManyBookingInput>
-
-    //     expect(productArray).toEqual('')
-    //   })
-    // })
+  //franchiseMasterUser = (await userService.find(franchise.id))!
+  agency = await accountService.create({
+    tenantId: franchise.id,
+    name: 'account1',
+    email: 'account1@gmail.com',
+    ownerId: 1,
+    category: 'Agency',
+    phone: '123121313'
   })
+  //agencyMasterUser = (await userService.find(agency.id))!
+})
+
+afterAll(() => {
+  moduleRef.close()
+})
+
+describe('new', () => {
+  it('should return a new booking object', async () => {
+    // const mock = jest
+    //   .spyOn(userService, 'getLoggedUser')
+    //   .mockImplementation(() =>
+    //     Promise.resolve({ ...agencyMasterUser, account: agency })
+    //   )
+
+    const booking = await bookingService.new()
+
+    expect(booking.id).toBeDefined()
+    expect(booking.tenantId).toEqual(franchise.id)
+    expect(booking.accountId).toEqual(agency.id)
+    expect(booking.status).toEqual(BookingStatus.WaitingService)
+    expect(booking.createdAt).toBeDefined()
+
+    jest.clearAllMocks()
+  })
+  // describe('', () => {
+  //   it('should ', async () => {
+  //     const booking = updateOneBooking(12)
+  //     const productArray = booking.data.products?.createMany
+  //       ?.data as Array<Prisma.BookingProductCreateManyBookingInput>
+
+  //     expect(productArray).toEqual('')
+  //   })
+  // })
 })
