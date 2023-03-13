1b7b36ee63a0061ba21689120b4b8562b067d5b8
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-03-13 16:53:05 -0300

    feat(server/user): add optional fields in create user dto file

--- libs/server/user/src/lib/dto/user.create.dto.ts
+++ libs/server/user/src/lib/dto/user.create.dto.ts
@@ -1,6 +1,20 @@
-import { IsString } from 'class-validator'
+import {
+  IsBoolean,
+  IsDate,
+  IsNumber,
+  IsOptional,
+  IsString
+} from 'class-validator'
 
 export class CreateUserDto {
+  @IsOptional()
+  @IsNumber()
+  tenantId?: number
+
+  @IsOptional()
+  @IsNumber()
+  accountId?: number
+
   @IsString()
   externalId: string
 
@@ -10,6 +24,66 @@ export class CreateUserDto {
   @IsString()
   lastName: string
 
+  @IsOptional()
+  @IsString()
+  companyName?: string
+
+  @IsOptional()
+  @IsString()
+  jobTitle?: string
+
+  @IsOptional()
+  @IsString()
+  department?: string
+
+  @IsOptional()
+  @IsString()
+  managerName?: string
+
   @IsString()
   email: string
+
+  @IsOptional()
+  @IsString()
+  phone?: string
+
+  @IsOptional()
+  @IsString()
+  mobilePhone?: string
+
+  @IsOptional()
+  @IsString()
+  fax?: string
+
+  @IsOptional()
+  @IsString()
+  gender?: string
+
+  @IsOptional()
+  @IsDate()
+  birthDate?: Date
+
+  @IsOptional()
+  @IsDate()
+  createdAt?: Date
+
+  @IsOptional()
+  @IsString()
+  createdBy?: string
+
+  @IsOptional()
+  @IsDate()
+  modifiedAt: Date
+
+  @IsOptional()
+  @IsString()
+  modifiedBy: string
+
+  @IsOptional()
+  @IsBoolean()
+  isActive: boolean
+
+  @IsOptional()
+  @IsBoolean()
+  isMaster: boolean
 }
