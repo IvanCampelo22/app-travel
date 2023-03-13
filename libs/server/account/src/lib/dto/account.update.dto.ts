ebe504c83d8e18bab423a4a863300cdbddeeb5ed
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-03-13 14:48:37 -0300

    feat(server/account): add new fields for update account

--- libs/server/account/src/lib/dto/account.update.dto.ts
+++ libs/server/account/src/lib/dto/account.update.dto.ts
@@ -1,28 +1,125 @@
 import { AccountCategory } from '@prisma/client'
-import { IsNumber, IsOptional, IsString } from 'class-validator'
+import {
+  IsBoolean,
+  IsDate,
+  IsNumber,
+  IsOptional,
+  IsString
+} from 'class-validator'
 
 export class UpdateAccountDto {
-  @IsOptional()
   @IsNumber()
   tenantId?: number
 
-  @IsOptional()
+  @IsNumber()
+  ownerId?: number
+
   @IsString()
   name?: string
 
-  @IsOptional()
   @IsString()
-  email?: string
+  category?: AccountCategory
 
   @IsOptional()
   @IsString()
-  category?: AccountCategory
+  dobName?: string
+
+  @IsOptional()
+  @IsString()
+  taxId?: string
+
+  @IsOptional()
+  @IsString()
+  ssn?: string
+
+  @IsOptional()
+  @IsString()
+  brand?: string
+
+  @IsString()
+  email?: string
 
   @IsOptional()
   @IsString()
   phone?: string
 
   @IsOptional()
-  @IsNumber()
-  ownerId?: number
+  @IsString()
+  mobilePhone?: string
+
+  @IsOptional()
+  @IsString()
+  fax?: string
+
+  @IsOptional()
+  @IsString()
+  termsAndCondition?: string
+
+  @IsOptional()
+  @IsString()
+  privacyPolicy?: string
+
+  @IsOptional()
+  @IsString()
+  bankName?: string
+
+  @IsOptional()
+  @IsString()
+  branch?: string
+
+  @IsOptional()
+  @IsString()
+  bankAccount?: string
+
+  @IsOptional()
+  @IsString()
+  notes?: string
+
+  @IsOptional()
+  @IsString()
+  countryCode?: string
+
+  @IsOptional()
+  @IsString()
+  currencyCode?: string
+
+  @IsOptional()
+  @IsString()
+  locale?: string
+
+  @IsOptional()
+  @IsString()
+  timeZone?: string
+
+  @IsOptional()
+  @IsString()
+  referralSource?: string
+
+  @IsOptional()
+  @IsString()
+  domain?: string
+
+  @IsOptional()
+  @IsString()
+  siteConfig?: string
+
+  @IsOptional()
+  @IsDate()
+  createdAt?: Date
+
+  @IsOptional()
+  @IsString()
+  createdBy?: string
+
+  @IsOptional()
+  @IsDate()
+  modifiedAt?: Date
+
+  @IsOptional()
+  @IsString()
+  modifiedBy?: string
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean
 }
