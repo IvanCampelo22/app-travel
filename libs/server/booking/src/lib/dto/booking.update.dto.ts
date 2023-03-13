6d9b50fa950332334159148647e0ff0780daf069
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-03-13 15:37:42 -0300

    Revert "feat(server/booking): add optional fields in create booking dto file"
    I added the fields to the booking update dto, thinking it was the create dto
    
    This reverts commit 3048dc09df6b34bdc62e001f3632308a3dc18859.

--- libs/server/booking/src/lib/dto/booking.update.dto.ts
+++ libs/server/booking/src/lib/dto/booking.update.dto.ts
@@ -1,25 +1,13 @@
-import { Decimal } from '@prisma/client/runtime'
-import {
-  IsBoolean,
-  IsDate,
-  IsDecimal,
-  IsNumber,
-  IsOptional,
-  IsString
-} from 'class-validator'
+import { IsNumber, IsOptional, IsString } from 'class-validator'
 
 export class UpdateBookingDto {
   @IsOptional()
   @IsNumber()
-  tenantId?: number
+  id: number
 
   @IsOptional()
   @IsNumber()
   accountId?: number
-
-  @IsOptional()
-  @IsNumber()
-  customerId?: number
 
   @IsOptional()
   @IsNumber()
@@ -36,104 +24,4 @@ export class UpdateBookingDto {
   @IsOptional()
   @IsString()
   customerPhone?: string
-
-  @IsOptional()
-  @IsString()
-  postalCode?: string
-
-  @IsOptional()
-  @IsString()
-  fromCity?: string
-
-  @IsOptional()
-  @IsString()
-  toCity?: string
-
-  @IsOptional()
-  @IsDate()
-  travelDate?: Date
-
-  @IsOptional()
-  @IsString()
-  travelPeriod?: string
-
-  @IsOptional()
-  @IsNumber()
-  adultsCount?: number
-
-  @IsOptional()
-  @IsNumber()
-  childrenCount?: number
-
-  @IsOptional()
-  @IsNumber()
-  seniorsCount?: number
-
-  @IsOptional()
-  @IsString()
-  requestDescription?: string
-
-  @IsOptional()
-  @IsString()
-  locatorCode?: string
-
-  @IsOptional()
-  @IsDecimal()
-  totalPrice?: Decimal
-
-  @IsOptional()
-  @IsDecimal()
-  totalCost?: Decimal
-
-  @IsOptional()
-  @IsString()
-  paymentType?: string
-
-  @IsOptional()
-  @IsString()
-  paymentStatus?: string
-
-  @IsOptional()
-  @IsDate()
-  paymentDateTime?: Date
-
-  @IsOptional()
-  @IsDecimal()
-  discountPercent?: Decimal
-
-  @IsOptional()
-  @IsDecimal()
-  discountAmount?: Decimal
-
-  @IsOptional()
-  @IsString()
-  voucherFilePath?: string
-
-  @IsOptional()
-  @IsString()
-  receiptFilePath?: string
-
-  @IsOptional()
-  @IsString()
-  invoiceFilePath?: string
-
-  @IsOptional()
-  @IsDate()
-  createdAt?: Date
-
-  @IsOptional()
-  @IsString()
-  createdBy?: string
-
-  @IsOptional()
-  @IsDate()
-  modifedAt?: Date
-
-  @IsOptional()
-  @IsString()
-  modifedBy?: string
-
-  @IsOptional()
-  @IsBoolean()
-  isActive?: boolean
 }
