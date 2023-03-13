e3f675b17e13fc9949c6d7def131bb7ccd657c52
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-03-13 16:55:33 -0300

    feat(server/user): add new fields in update user dto

--- libs/server/user/src/lib/dto/user.update.dto.ts
+++ libs/server/user/src/lib/dto/user.update.dto.ts
@@ -1,6 +1,20 @@
-import { IsOptional, IsString } from 'class-validator'
+import {
+  IsBoolean,
+  IsDate,
+  IsNumber,
+  IsOptional,
+  IsString
+} from 'class-validator'
 
 export class UpdateUserDto {
+  @IsOptional()
+  @IsNumber()
+  tenantId?: number
+
+  @IsOptional()
+  @IsNumber()
+  accountId?: number
+
   @IsOptional()
   @IsString()
   externalId?: string
@@ -15,5 +29,65 @@ export class UpdateUserDto {
 
   @IsOptional()
   @IsString()
+  companyName?: string
+
+  @IsOptional()
+  @IsString()
+  jobTitle?: string
+
+  @IsOptional()
+  @IsString()
+  department?: string
+
+  @IsOptional()
+  @IsString()
+  managerName?: string
+
+  @IsOptional()
+  @IsString()
   email?: string
+
+  @IsOptional()
+  @IsString()
+  phone?: string
+
+  @IsOptional()
+  @IsString()
+  mobilePhone?: string
+
+  @IsOptional()
+  @IsString()
+  fax?: string
+
+  @IsOptional()
+  @IsString()
+  gender?: string
+
+  @IsOptional()
+  @IsDate()
+  birthDate?: Date
+
+  @IsOptional()
+  @IsDate()
+  createdAt?: Date
+
+  @IsOptional()
+  @IsString()
+  createdBy?: string
+
+  @IsOptional()
+  @IsDate()
+  modifiedAt?: Date
+
+  @IsOptional()
+  @IsString()
+  modifiedBy?: string
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean
+
+  @IsOptional()
+  @IsBoolean()
+  isMaster?: boolean
 }
