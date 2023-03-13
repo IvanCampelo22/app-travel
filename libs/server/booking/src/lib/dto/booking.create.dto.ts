9099e304f02fa3252b0465969894c201212f4435
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-03-13 15:51:00 -0300

    feat(server/booking): add optional fields in file create booking dto

--- libs/server/booking/src/lib/dto/booking.create.dto.ts
+++ libs/server/booking/src/lib/dto/booking.create.dto.ts
@@ -1,6 +1,18 @@
-import { IsNumber, IsOptional, IsString } from 'class-validator'
+import { Decimal } from '@prisma/client/runtime'
+import {
+  IsBoolean,
+  IsDate,
+  IsDecimal,
+  IsNumber,
+  IsOptional,
+  IsString
+} from 'class-validator'
 
 export class CreateBookingDto {
+  @IsOptional()
+  @IsNumber()
+  tenantId?: number
+
   @IsNumber()
   accountId: number
 
@@ -19,4 +31,104 @@ export class CreateBookingDto {
   @IsOptional()
   @IsString()
   customerPhone?: string
+
+  @IsOptional()
+  @IsString()
+  postalCode?: string
+
+  @IsOptional()
+  @IsString()
+  fromCity?: string
+
+  @IsOptional()
+  @IsString()
+  toCity?: string
+
+  @IsOptional()
+  @IsDate()
+  travelDate?: Date
+
+  @IsOptional()
+  @IsString()
+  travelPeriod?: string
+
+  @IsOptional()
+  @IsNumber()
+  adultsCount?: number
+
+  @IsOptional()
+  @IsNumber()
+  childrenCount?: number
+
+  @IsOptional()
+  @IsNumber()
+  seniorsCount?: number
+
+  @IsOptional()
+  @IsString()
+  requestDescription?: string
+
+  @IsOptional()
+  @IsString()
+  locatorCode?: string
+
+  @IsOptional()
+  @IsDecimal()
+  totalPrice?: Decimal
+
+  @IsOptional()
+  @IsDecimal()
+  totalCost?: Decimal
+
+  @IsOptional()
+  @IsString()
+  paymentType?: string
+
+  @IsOptional()
+  @IsString()
+  paymentStatus?: string
+
+  @IsOptional()
+  @IsDate()
+  paymentDateTime?: Date
+
+  @IsOptional()
+  @IsDecimal()
+  discountPercent?: Decimal
+
+  @IsOptional()
+  @IsDecimal()
+  discountAmount?: Decimal
+
+  @IsOptional()
+  @IsString()
+  voucherFilePath?: string
+
+  @IsOptional()
+  @IsString()
+  receiptFilePath?: string
+
+  @IsOptional()
+  @IsString()
+  invoiceFilePath?: string
+
+  @IsOptional()
+  @IsDate()
+  createdAt?: Date
+
+  @IsOptional()
+  @IsString()
+  createdBy?: string
+
+  @IsOptional()
+  @IsDate()
+  modifedAt?: Date
+
+  @IsOptional()
+  @IsString()
+  modifedBy?: string
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean
 }
