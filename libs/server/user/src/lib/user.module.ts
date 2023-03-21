b67441d452a7160143f79161f865df2ab4e634de
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-03-21 10:23:01 -0300

    fix(server/users): add user controllers in user modules

--- libs/server/user/src/lib/user.module.ts
+++ libs/server/user/src/lib/user.module.ts
@@ -1,8 +1,10 @@
 import { Global, Module } from '@nestjs/common'
+import { UserControllers } from './user.controllers'
 import { UserService } from './user.service'
 
 @Global()
 @Module({
+  controllers: [UserControllers],
   providers: [UserService],
   exports: [UserService]
 })
