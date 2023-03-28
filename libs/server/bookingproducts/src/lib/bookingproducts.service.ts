38b2169d6c81cbd38aae8aabbc9d3b80787106dd
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-03-28 15:22:54 -0300

    feat(server/bookingproduct): add update function in service

--- libs/server/bookingproducts/src/lib/bookingproducts.service.ts
+++ libs/server/bookingproducts/src/lib/bookingproducts.service.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common'
 import { DatabaseService } from '@server/database'
 import { CreateBookingProductDto } from './dto/bookingproduct.create.dto'
+import { UpdateBookingProductDto } from './dto/bookingproduct.update.dto'
 
 @Injectable()
 export class BookingProductService {
@@ -25,6 +26,14 @@ export class BookingProductService {
   async find(id: number) {
     try {
       return this.service.bookingProduct.findUnique({ where: { id } })
+    } catch (error) {
+      throw new NotFoundException('Not Found')
+    }
+  }
+
+  async update(id: number, input: UpdateBookingProductDto) {
+    try {
+      return this.service.bookingProduct.update({ where: { id }, data: input })
     } catch (error) {
       throw new NotFoundException('Not Found')
     }
