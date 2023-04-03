34f94b06c0833a0395d5ac62b3ba768306bf8326
sal-rosa <danielvasconcelos148@gmail.com>
2023-04-02 22:42:22 -0300

    feat(pages/costumer-name): add pagination, filter date and delete row

--- apps/web/pages/admin/customer-service/index.tsx
+++ apps/web/pages/admin/customer-service/index.tsx
@@ -29,17 +29,31 @@ import { SideNavbar } from '@web/side-navbar'
 import { TableRow } from '@web/table-row'
 
 import { AdminLayout, Meta, NextPageWithLayout } from '@web/base-ui'
+import { useState } from 'react'
 
 const queryClient = new QueryClient()
+
+const today = new Date()
+
+const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1)
+const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0)
 
 const IndexPage: NextPageWithLayout = () => {
   const API_URL = process.env.NEXT_PUBLIC_SERVER_URL
 
-  const { data } = useQuery(['bookings'], () =>
-    fetch(`${API_URL}/bookings`)
-      .then((res) => res.json())
-      .catch(() => alert('Erro ao buscar atendimentos'))
-  )
+  const [dates, setDates] = useState<[Date | null, Date | null]>([firstDayOfMonth, lastDayOfMonth]);
+  const [page, setPage] = useState<number>(1);
+
+  const getBookings = useQuery(['getBookings', page], {
+    staleTime: Infinity,
+    cacheTime: 0,
+    refetchOnMount: 'always',
+    queryFn: () => {
+      return fetch(`${API_URL}/bookings/?start_date='${dates[0]}'&end_date='${dates[1]}'&page=${page}`)
+        .then((res) => res.json())
+        .catch(() => alert('Erro ao buscar atendimentos'))
+    }
+  })
 
   const deleteBooking = useMutation((bookingId) => {
     return fetch(`${API_URL}/bookings/${bookingId}`, {
@@ -51,6 +65,37 @@ const IndexPage: NextPageWithLayout = () => {
       })
       .catch(() => alert('Erro ao deletar atendimento'))
   })
+
+  const formatDateMonthDayYear = (dateString) => {
+    const date = new Date(dateString)
+    const monthNames = [
+      "Jan", "Feb", "Mar",
+      "Apr", "May", "Jun", "Jul",
+      "Aug", "Sep", "Oct",
+      "Nov", "Dec"
+    ]
+
+    const day = date.getDate()
+    const monthIndex = date.getMonth()
+    const year = date.getFullYear()
+
+    return `${monthNames[monthIndex]} ${day}, ${year}`
+  }
+
+  const formatDateDayMonthYearTime = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getUTCFullYear();
+    const month = date.toLocaleString('en-US', { month: 'short' }).toUpperCase();
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const hour = String(date.getUTCHours()).padStart(2, '0');
+    const minute = String(date.getUTCMinutes()).padStart(2, '0');
+    const second = String(date.getUTCSeconds()).padStart(2, '0');
+    const formattedDateString = `${year}${month}${day}${hour}${minute}${second}`;
+
+    return formattedDateString;
+  };
+
+  if (getBookings.isLoading) return <p>Loading...</p>
 
   return (
     <Flex>
@@ -80,7 +125,7 @@ const IndexPage: NextPageWithLayout = () => {
           </Link>
         </Header>
         <HorizontalTabs mt={60} mb="xl" />
-        <Filter />
+        <Filter dates={dates} onChangeDate={setDates} onClickFilter={() => { getBookings.refetch() }} />
         <Paper withBorder mt="lg" shadow="lg">
           <Box p="md">
             <Title order={4} color="gray.9">
@@ -106,26 +151,27 @@ const IndexPage: NextPageWithLayout = () => {
                 </tr>
               </thead>
               <tbody>
-                {data
-                  ? data.map((row) => {
-                      return (
-                        <TableRow
-                          key={row.id}
-                          id={row.id}
-                          customer={row.customerName}
-                          customerEmail={row.customerEmail}
-                          status={row.status.replace(/([A-Z])/g, ' $1')}
-                          date={row.createdAt}
-                          onClickDelete={() => deleteBooking.mutate(row.id)}
-                        />
-                      )
-                    })
+                {getBookings.data && getBookings.data !== null
+                  ? getBookings.data?.filter((booking) => booking.isActive).map((booking) => {
+                    return (
+                      <TableRow
+                        key={booking.id}
+                        createdAt={formatDateDayMonthYearTime(booking.createdAt)}
+                        id={booking.id}
+                        customer={booking.customerName}
+                        customerEmail={booking.customerEmail}
+                        status={booking.status.replace(/([A-Z])/g, ' $1')}
+                        date={formatDateMonthDayYear(booking.createdAt)}
+                        onClickDelete={() => { deleteBooking.mutate(booking.id) }}
+                      />
+                    )
+                  })
                   : null}
               </tbody>
             </Table>
           </ScrollArea>
           <Divider color="gray.3" />
-          <Pagination p="md" />
+          <Pagination onClickNext={() => setPage(page + 1)} onClickPrev={() => { page > 1 ? setPage(page - 1) : null }} />
         </Paper>
       </Container>
     </Flex>
