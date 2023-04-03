354e8adc87444df207e02356e9ddb76ab985c2cb
sal-rosa <danielvasconcelos148@gmail.com>
2023-04-02 22:51:23 -0300

    feat(web/pagination): add pagination event

--- libs/web/pagination/src/lib/pagination.tsx
+++ libs/web/pagination/src/lib/pagination.tsx
@@ -1,26 +1,21 @@
-import {
-  Button,
-  Flex,
-  Group,
-  MantineStyleSystemProps,
-  Text
-} from '@mantine/core'
+import { Button, Flex, Group, Text } from '@mantine/core'
 
-interface PaginationProps extends MantineStyleSystemProps {
-  default?: string
+interface PaginationProps {
+  onClickPrev?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  onClickNext?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 export function Pagination(props: PaginationProps) {
   return (
-    <Flex {...props} justify="space-between" align="center">
+    <Flex p="md" justify="space-between" align="center">
       <Text weight="500" color="gray.7">
         Page 1 of 10
       </Text>
       <Group>
-        <Button variant="default" radius="md">
+        <Button variant="default" radius="md" onClick={props.onClickPrev}>
           Previus
         </Button>
-        <Button variant="default" radius="md">
+        <Button variant="default" radius="md" onClick={props.onClickNext}>
           Next
         </Button>
       </Group>
