e31fd5f1194c90802e41d792e46d87a1b29b4161
FabianoFSantos <91429468+FabianoFSantos@users.noreply.github.com>
2023-04-04 16:30:16 -0300

    Update main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:4200',
-      'https://backend.dev.viagem10.com/',
+      'http://web.dev.viagem10.com',
       'https://viagem10-monorepo-git-main-fabianofsantos.vercel.app',
       'https://viagem10-monorepo.vercel.app'
     ],
